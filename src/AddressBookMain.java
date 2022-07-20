c770922758060d543da3216ba4334b7b7bce1581
MayurBari <barimayur1265@gmail.com>
2022-07-19 21:44:28 -0700

    Added:JDBCAddressBook

---
+++ src/AddressBookMain.java
@@ -0,0 +1,36 @@
+ import java.util.*;
+
+    public class AddressBookMain
+    {
+        static Scanner scanner = new Scanner(System.in);
+
+        public static void main(String[] args) {
+
+            boolean exit = false;
+            while (!exit) {
+                System.out.println(" Press\n 1 ->  Retrieve data\n 2 -> Exit");
+                switch (scanner.nextInt()) {
+                    case 1:
+                        retrieveData();
+                        break;
+                    case 2:
+                        exit = true;
+                }
+            }
+        }
+
+        private static void retrieveData()
+        {
+            AddressBook addressBookRepo = new AddressBook();
+            List<Contacts> employeeInfoList = addressBookRepo.retrieveData();
+            for (Contacts employee : employeeInfoList
+            ) {
+                System.out.println(employee + "\n");
+            }
+        }
+        private static void updateCity() {
+            AddressBook addressBookRepo = new AddressBook();
+            System.out.println("Enter the address,city,state, zip and Serial Number  to Update");
+            addressBookRepo.updateCityByZip(scanner.next(), scanner.next(), scanner.next(), scanner.nextInt(), scanner.nextInt());
+        }
+}
